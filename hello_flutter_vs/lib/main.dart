1e0fc62ac2e2ae7c51c5f29f0a0cb0b110e7cdad
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-09 22:00:59 +0530

    - initial commit

---
+++ hello_flutter_vs/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Hello Flutter App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Title in App Bar"),
+      ),
+      body: Material(
+        color: Colors.deepPurple,
+        child: Center(
+          child: Text(
+            "Hello Flutter!",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 36.0),
+          ),
+        ),
+      ),
+    ),
+  ));
+}
