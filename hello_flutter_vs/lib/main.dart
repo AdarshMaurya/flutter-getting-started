fa93ea8c4071861d7196d094fbddcba24d8ab824
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-09 22:23:36 +0530

    - organizing code: class in dart

--- hello_flutter_vs/lib/main.dart
+++ hello_flutter_vs/lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Hello Flutter App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Title in App Bar"),
-      ),
-      body: Material(
-        color: Colors.deepPurple,
-        child: Center(
-          child: Text(
-            "Hello Flutter!",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 36.0),
+  runApp(new HelloFlutterApp());
+}
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title in App Bar"),
+        ),
+        body: Material(
+          color: Colors.deepPurple,
+          child: Center(
+            child: Text(
+              "Hello Flutter!",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 36.0),
+            ),
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
