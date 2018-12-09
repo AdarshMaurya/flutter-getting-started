6cb0505249e2511e36d7f580ca808f3ba87ff566
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-09 22:28:16 +0530

    - fat arrow

--- hello_flutter_vs/lib/main.dart
+++ hello_flutter_vs/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new HelloFlutterApp());
-}
+void main() => runApp(new HelloFlutterApp());
 
 class HelloFlutterApp extends StatelessWidget {
   @override
