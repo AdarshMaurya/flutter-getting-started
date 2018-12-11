d27274f0a462aaca11413f7fdaca9805da1f8f29
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-12 04:48:29 +0530

    - added InputDecoration

--- trip_cost_app/lib/main.dart
+++ trip_cost_app/lib/main.dart
@@ -35,6 +35,9 @@ class _HelloYouState extends State<HelloYou> {
           child: Column(
             children: <Widget>[
               TextField(
+                decoration: InputDecoration(
+                  hintText: 'Please insert your name'
+                ),
                 onChanged: (String string) {
                   setState(() {
                     name = string;
