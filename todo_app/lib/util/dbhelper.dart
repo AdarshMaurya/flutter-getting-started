e70ac846db716d583c4f16c7b8c1addaa7949ccd
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-16 03:37:38 +0530

    - create sqflite database with future async and wait methods

--- todo_app/lib/util/dbhelper.dart
+++ todo_app/lib/util/dbhelper.dart
@@ -18,4 +18,26 @@ class DbHelper {
   factory DbHelper() {
     return _dbbHelper;
   }
+
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initializeDb();
+    }
+    return _db;
+  }
+
+  Future<Database> initializeDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + "todos.db";
+    var dbTodos = await openDatabase(path, version: 1, onCreate: _createDb);
+    return dbTodos;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tblTodo($colId INETEGER PRIMARY KEY, $colTitle TEXT," +
+            "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)");
+  }
 }
