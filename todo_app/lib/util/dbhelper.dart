e7054e051fe476bd77c19620a065076966cc09ff
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-16 03:23:21 +0530

    - added pubspec dependencies: sqflite, path_provides, intl. - added dbhelper.dart file

---
+++ todo_app/lib/util/dbhelper.dart
@@ -0,0 +1,21 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_app/model/todo.dart';
+
+class DbHelper {
+  static final DbHelper _dbbHelper = DbHelper._internal();
+  String tblTodo = "todo";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
+
+  DbHelper._internal();
+
+  factory DbHelper() {
+    return _dbbHelper;
+  }
+}
