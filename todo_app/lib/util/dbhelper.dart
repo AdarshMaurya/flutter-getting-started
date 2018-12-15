e8b0124d2ec0ffc6f4e8921b70255db6a1ce0161
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-16 03:52:04 +0530

    - creating dbHelper : CRUD operation

--- todo_app/lib/util/dbhelper.dart
+++ todo_app/lib/util/dbhelper.dart
@@ -40,4 +40,38 @@ class DbHelper {
         "CREATE TABLE $tblTodo($colId INETEGER PRIMARY KEY, $colTitle TEXT," +
             "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)");
   }
+
+  Future<int> insertTodo(Todo todo) async {
+    Database db = await this.db;
+    var result = await db.insert(tblTodo, todo.toMap());
+    return result;
+  }
+
+  Future<List> getTodos() async {
+    Database db = await this.db;
+    var result =
+        await db.rawQuery("SELECT * FROM $tblTodo order bt $colPriority ASC");
+    return result;
+  }
+
+  Future<int> getCount() async {
+    Database db = await this.db;
+    var result = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT COUNT (*) FROM $tblTodo"));
+    return result;
+  }
+
+  Future<int> updateTodo(Todo todo) async {
+    var db = await this.db;
+    var result = await db
+        .update(tblTodo, todo.toMap(), where: "$colId=?", whereArgs: [todo.id]);
+    return result;
+  }
+
+  Future<int> deleteTodo(int id) async {
+    int result;
+    var db = await this.db;
+    result = await db.rawDelete("DELETE FROM $tblTodo WHERE $colId=$id");
+    return result;
+  }
 }
