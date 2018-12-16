b1f2eac529a5c11885a20ebf41e651ab25db3be2
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-16 11:46:47 +0530

    - added onTap, onLongPress, onDoubleTap gestures. Added Horizontal & VerticalDragUpdate

--- square/lib/main.dart
+++ square/lib/main.dart
@@ -30,12 +30,60 @@ class _MyHomePageState extends State<MyHomePage> {
   int numDoubleTaps = 0;
   int numLongPress = 0;
 
+  double posX = 0.0;
+  double posY = 0.0;
+  double boxSize = 150.0;
+
   @override
   Widget build(BuildContext context) {
+    if (posX == 0.0) {
+      center(context);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text("Gestures and Animations"),
       ),
+      body: GestureDetector(
+          onTap: () {
+            setState(() {
+              numTaps++;
+            });
+          },
+          onDoubleTap: () {
+            setState(() {
+              numDoubleTaps++;
+            });
+          },
+          onLongPress: () {
+            setState(() {
+              numLongPress++;
+            });
+          },
+          onVerticalDragUpdate: (DragUpdateDetails value) {
+            setState(() {
+              double delta = value.delta.dy;
+              posY += delta;
+            });
+          },
+          onHorizontalDragUpdate: (DragUpdateDetails value) {
+            setState(() {
+              double delta = value.delta.dx;
+              posX += delta;
+            });
+          },
+          child: Stack(children: <Widget>[
+            Positioned(
+              left: posX,
+              top: posY,
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ])),
       bottomNavigationBar: Material(
         color: Theme.of(context).primaryColorLight,
         child: Padding(
@@ -47,5 +95,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.width / 2) - boxSize / 2 - 30.0;
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
   }
 }
