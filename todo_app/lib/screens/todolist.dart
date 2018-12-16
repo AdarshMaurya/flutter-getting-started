e162a6896638ac9cb950971205b558145a1416d2
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-16 06:21:57 +0530

    - adding navigation

--- todo_app/lib/screens/todolist.dart
+++ todo_app/lib/screens/todolist.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/model/todo.dart';
 import 'package:todo_app/util/dbhelper.dart';
+import 'package:todo_app/screens/tododetail.dart';
 
 class TodoList extends StatefulWidget {
   @override
@@ -20,7 +21,9 @@ class TodoListState extends State {
     return Scaffold(
       body: todoListItems(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: () {
+          navigateToDetail(Todo('', 3, ''));
+        },
         tooltip: "Add new Todo",
         child: new Icon(Icons.add),
       ),
@@ -42,6 +45,7 @@ class TodoListState extends State {
               subtitle: Text(this.todos[position].date),
               onTap: () {
                 debugPrint("Tapped on " + this.todos[position].id.toString());
+                navigateToDetail(this.todos[position]);
               }),
         );
       },
@@ -82,5 +86,12 @@ class TodoListState extends State {
       default:
         return Colors.green;
     }
+  }
+
+  void navigateToDetail(Todo todo) async {
+    bool result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TodoDetail(todo)),
+    );
   }
 }
